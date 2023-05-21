59ba97b1a87d0985a78c810b88130ebb2b7ed9a2
Filimonov Kirill <taik65523@gmail.com>
2023-05-21 10:54:36 +0300

    Ну так получилось

--- IDictionary.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public interface IDictionary {
-    public void loadFile() throws IOException;
-    public void Add(String value) throws IOException;
-    public void Remove(String key) throws IOException;
-    public void ReadAll();
-    public void Search(String key);
-}
